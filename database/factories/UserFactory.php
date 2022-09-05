97a7568213be22e598dfc89f492a06114ab56065
Daniel <daniel.garcia@issivs.com>
2022-09-05 18:29:42 -0500

    agregando seed admin

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,11 +18,11 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'ubicacion' => '123',
+            'admin' => 1,
+            'password' => '$2y$10$YkZAJEzwgNewD9ik5HVbXec7dQrG8MUKys.gpqaFP0ZR0IcOzoSvi', // password
         ];
     }
 
--- database/seeders/ClimaSeeder.php
+++ database/seeders/ClimaSeeder.php
@@ -14,6 +14,5 @@ class ClimaSeeder extends Seeder
      */
     public function run()
     {
-        //
     }
 }
